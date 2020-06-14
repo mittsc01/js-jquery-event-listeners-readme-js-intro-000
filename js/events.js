25fad0ccc0b8c2f9898f29bc4bcc0f4655981794
Scott Mittman <sm2884@nau.edu>
2020-06-14 20:47:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,20 @@
-//define functions here
+function getIt(){
+  $('p').on('click',alert("Hey!"))
+}
+function frameIt(){
+  $('img').on('load',()=>{$('img').addClass('tasty')})
+}
+function pressIt(){
+  $('input').on('keydown',(e)=>{
+    if (e.which == 71){
+      alert('G was pressed.')
+    }
+  })
+
+}
+function submitIt(){
+  $('form').on('submit',alert("Your form is going to be submitted now."))
+}
 
 $(document).ready(function(){
 
